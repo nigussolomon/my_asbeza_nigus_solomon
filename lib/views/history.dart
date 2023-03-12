2086fc8991b7b28da297f60972850aecf709de14
nigussolomon <nigussolomon444@gmail.com>
2023-03-12 03:36:53 +0300

    added the history ui and price calculator

--- lib/views/history.dart
+++ lib/views/history.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_asbeza/views/components/myDrawer.dart';
 
+import '../bloc/asbeza_bloc.dart';
 import 'components/myAppBar.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -16,8 +18,120 @@ class _HistoryPageState extends State<HistoryPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(100), child: MyAppBar()),
+          preferredSize: Size.fromHeight(50), child: MyAppBar()),
       drawer: MyDrawer(),
+      body: BlocBuilder<AsbezaBloc, AsbezaState>(
+        builder: (context, state) {
+          if (state is AsbezaInitial) {
+            return const Center(
+                child: Text(
+              "NO HISTORY TO SHOW!\n TOTAL: 0\$",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+              ),
+            ));
+          }
+          if (state is AsbezaLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is AsbezaSuccess) {
+            if (state.history.isEmpty) {
+              return const Center(
+                  child: Text(
+                "NO HISTORY TO SHOW!\n TOTAL: 0\$",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                ),
+              ));
+            } else {
+              // ignore: non_constant_identifier_names
+              num TotalPrice = 0;
+              // ignore: no_leading_underscores_for_local_identifiers
+              void _incrementCounter() {
+                for (var element in state.history) {
+                  TotalPrice += element.foodPrice;
+                }
+              }
+
+              _incrementCounter();
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "TOTAL: ${TotalPrice.toStringAsFixed(2)}\$",
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    height: MediaQuery.of(context).size.height * .84,
+                    child: ListView.builder(
+                      itemCount: state.history.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final asbezaVal = state.history[index];
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              fit: BoxFit.contain,
+                                              image: NetworkImage(
+                                                  asbezaVal.image))),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              .1,
+                                      width: MediaQuery.of(context).size.width *
+                                          .3,
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 11, vertical: 5),
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          .4,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(asbezaVal.foodTitle),
+                                          Text(
+                                            "${asbezaVal.foodPrice}\$",
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.w900),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
