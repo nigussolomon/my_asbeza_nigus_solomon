a7fd081a4eda62b234be1b4c1458d6dfabc94b11
nigussolomon <nigussolomon444@gmail.com>
2023-03-12 03:35:40 +0300

    fixed event bug that refetches on every bloc call

--- lib/bloc/asbeza_event.dart
+++ lib/bloc/asbeza_event.dart
@@ -1,8 +1,25 @@
 part of 'asbeza_bloc.dart';
 
-class AsbezaEvent extends Equatable {
+abstract class AsbezaEvent extends Equatable {
   const AsbezaEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class AsbezaFetchEvent extends AsbezaEvent {
+  const AsbezaFetchEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class HistoryEvent extends AsbezaEvent {
+  final Asbeza asbeza;
+  const HistoryEvent({required this.asbeza});
+
+  @override
+  List<Object> get props => [];
+
+  get data => asbeza;
 }
